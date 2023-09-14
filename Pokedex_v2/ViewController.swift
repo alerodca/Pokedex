18fb0d229425e9016c376559058359c7081f3668
Alejandro Rodríguez Cañete <ale.rodca04@gmail.com>
2023-09-14 10:57:53 +0200

    Initial Commit

---
+++ Pokedex_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex_v2
+//
+//  Created by Alejandro Rodríguez Cañete on 14/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
